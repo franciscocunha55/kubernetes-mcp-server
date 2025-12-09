771e7cb0b8504ddb37fc5a93edbcde27d41e2427
franciscocunha55 <afrancisco97@gmail.com>
2025-12-09 12:17:10 +0000

    add mcp server initial config

--- main.go
+++ main.go
@@ -1,8 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+
+	"github.com/mark3labs/mcp-go/server"
+)
+
 
 func main() {
 	// This is a placeholder for the main function.
 	fmt.Println("Hello, World!")
+
+	mcpServer := server.NewMCPServer(
+		"Kubernetes MCP Server",
+		"1.0.0",
+		server.WithToolCapabilities(true),
+		server.WithResourceCapabilities(true, true),
+	)
+
+	streamableHTTPServer := server.NewStreamableHTTPServer(mcpServer)
+	log.Printf("Starting MCP server on HTTP at port :8080...")
+	log.Printf("MCP endpoint will be available at: http://localhost:8080/mcp")
+	if err:=streamableHTTPServer.Start(":8080"); err != nil {
+		log.Fatalf("Failed to start MCP server: %v", err)
+	}
+	
 }
